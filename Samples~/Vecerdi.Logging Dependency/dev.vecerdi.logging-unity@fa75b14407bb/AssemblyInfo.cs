ebd06881a4459e9483259a506834394bc21220a1
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2025-05-04 10:13:15 +0300

    Remove the Vecerdi.Logging Dependency package sample

--- Samples~/Vecerdi.Logging Dependency/dev.vecerdi.logging-unity@fa75b14407bb/AssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Vecerdi.Logging.Unity.Editor")]
--- Samples~/Vecerdi.Logging Dependency/dev.vecerdi.logging-unity@fa75b14407bb/Settings/CustomProjectSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.IO;
-using UnityEngine;
-
-namespace Vecerdi.Logging.Unity {
-    internal static class CustomProjectSettings {
-        public static string ProjectPath { get; } = Directory.GetParent(Application.dataPath)!.FullName;
-        public static string ProjectSettingsPath { get; } = Path.Combine(ProjectPath, "Library/Vecerdi.Logging");
-    }
-}
